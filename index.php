8eb45fc2ce845b154dd28652106b3ad8731fcd2e
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-28 15:52:19 +0100

    esercizio + bonus 1

--- index.php
+++ index.php
@@ -39,6 +39,19 @@ $hotels = [
 
 ];
 
+if (isset($_GET['type']) && !empty($_GET['type'])) {
+    // var_dump($_GET);
+    $temp = [];
+    foreach ($hotels as $item) {
+
+        if ($item['parking'] == true) {
+            $temp[] = $item;
+        }
+    }
+    ;
+    $hotels = $temp;
+    // var_dump($hotels);
+}
 ?>
 <!DOCTYPE html>
 <html lang="it">
@@ -52,11 +65,51 @@ $hotels = [
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>PHP hotel</title>
 </head>
 
 <body>
+    <div class="container">
+        <form action="index.php" method="GET" name="filtro">
+            <div class="mb-3 mt-3">
+                <label for="type">
+                    <select name="type" id="">
+                        <option value="">Scegli</option>
+                        <option value="parking">Parcheggio</option>
+                        <!-- <option value="false">Votazione</option> -->
+                    </select>
+                    <div> <button type="submit" class="btn btn-secondary mt-3">invia</button></div>
 
+                </label>
+            </div>
+        </form>
+        <table class='table'>
+            <thead>
+                <tr>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Parking</th>
+                    <th scope='col'>Vote</th>
+                    <th scope='col'>Distance_to_center</th>
+                </tr>
+            </thead>
+            <?php foreach ($hotels as $item) {
+                $park = $item['parking'] ? 'si' : 'no';
+                ;
+                echo
+                    "<tbody>
+                <tr>
+                    <td>'$item[name]'</td>
+                    <td>'$item[description]'</td>
+                    <td>'$park'</td>
+                    <td>'$item[vote]'</td>
+                    <td>'$item[distance_to_center]'</td>
+                </tr>
+            </tbody>";
+
+            } ?>
+        </table>
+    </div>
 </body>
 
 </html>
